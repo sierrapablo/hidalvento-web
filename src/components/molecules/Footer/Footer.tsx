8af14b145c5cbb00654f520f2f16d4ddbda3721b
sierrapablo <pablosierralorente@outlook.es>
2025-04-03 04:46:18 +0200

    🎨 code refactoring: Refactor Footer

---
+++ src/components/molecules/Footer/Footer.tsx
@@ -0,0 +1,60 @@
+import { Facebook, X, Instagram, LinkedIn } from '@mui/icons-material';
+import { SocialIcon } from '../../atoms/SocialIcon/SocialIcon';
+import { Link } from 'react-router-dom';
+import { footerStyles, footerContainerStyles, columnContainerStyles, logoContainerStyles, iconStyles, socialsContainerStyles, labelStyles, titleStyles } from './footerStyles';
+
+export const Footer = () => {
+  return (
+    <footer className={footerStyles()}>
+      <div className={footerContainerStyles()}>
+
+        {/* Logo y Redes Sociales */}
+        <div className={columnContainerStyles()}>
+          <div className={logoContainerStyles()}>
+            <img src="" alt="HidalVento" className={iconStyles()} />
+          </div>
+          <div className={socialsContainerStyles()}>
+            <SocialIcon Icon={Facebook} href="https://facebook.com" />
+            <SocialIcon Icon={X} href="https://twitter.com" />
+            <SocialIcon Icon={Instagram} href="https://instagram.com" />
+            <SocialIcon Icon={LinkedIn} href="https://linkedin.com" />
+          </div>
+        </div>
+
+        {/* Tarjeta de contacto */}
+        <div className={columnContainerStyles()}>
+          <a className={titleStyles()}>Contacto</a>
+          <a href="" className={labelStyles()}>
+            Pl. Julio Verne 2, Esc. B, 7<br />
+            46017 Valencia, España
+          </a>
+          <a href="" className={labelStyles()}>+34 666 08 98 52</a>
+          <a href="" className={labelStyles()}>hola@hidalvento.es</a>
+          <Link to="/contacto" className={labelStyles()}>Contacto</Link>
+        </div>
+
+        {/* Links principales */}
+        <div className={columnContainerStyles()}>
+          <a className={titleStyles()}>Cómo podemos ayudarte</a>
+          <Link to="/" className={labelStyles()}>Inicio</Link>
+          <Link to="/sobre-nosotros" className={labelStyles()}>Sobre nosotros</Link>
+          <Link to="/servicios" className={labelStyles()}>Servicios</Link>
+          <Link to="/galeria" className={labelStyles()}>Galería</Link>
+        </div>
+
+        {/* Links a servicios */}
+        <div className={columnContainerStyles()}>
+          <a className={titleStyles()}>Servicios</a>
+          <a href="" className={labelStyles()}>Reformas</a>
+          <a href="" className={labelStyles()}>Fontanería</a>
+          <a href="" className={labelStyles()}>Electricidad</a>
+          <a href="" className={labelStyles()}>Pintura</a>
+          <a href="" className={labelStyles()}>Ventanas</a>
+          <a href="" className={labelStyles()}>Suelo</a>
+          <a href="" className={labelStyles()}>Ducha</a>
+        </div>
+
+      </div>
+    </footer >
+  );
+};
---
+++ src/components/molecules/Footer/footerStyles.ts
@@ -0,0 +1,33 @@
+import { tv } from 'tailwind-variants';
+
+export const footerStyles = tv({
+  base: "bg-hv-blue lg:px-40 py-10 w-full"
+});
+
+export const footerContainerStyles = tv({
+  base: "grid md:grid-cols-4 md:justify-items-center gap-4"
+});
+
+export const columnContainerStyles = tv({
+  base: "flex flex-col m-4"
+});
+
+export const iconStyles = tv({
+  base: ""
+});
+
+export const labelStyles = tv({
+  base: "text-sm text-gray-300 hover:text-white transition wrap"
+});
+
+export const titleStyles = tv({
+  base: "font-bold text-white mb-2"
+});
+
+export const logoContainerStyles = tv({
+  base: ""
+});
+
+export const socialsContainerStyles = tv({
+  base: "flex md:space-x-4 md:mt-4"
+});

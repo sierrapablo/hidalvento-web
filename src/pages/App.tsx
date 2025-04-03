e268a34881efa8e05349194742dc1cbdd981f1fb
sierrapablo <pablosierralorente@outlook.es>
2025-04-03 05:08:21 +0200

    🗑️ remove: remove appcontainer styles

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,18 @@
 import { Navbar } from '../components/molecules/Navbar/Navbar';
 import { Footer } from '../components/molecules/Footer/Footer';
-import { tv } from 'tailwind-variants';
 import { HeroSection } from '../components/templates/HeroSection/HeroSection';
-
-const appcontainer = tv({
-  base: "flex flex-col min-h-screen grow"
-});
 
 export const App = () => {
   return (
-    <div className={appcontainer()}>
+    <>
       <Navbar />
-      <HeroSection subtitle={{ text: "Tu reforma comienza aquí", variant: "secondary" }} />
+      <HeroSection
+        subtitle={{
+          text: "Tu reforma comienza aquí",
+          variant: "secondary"
+        }}
+      />
       <Footer />
-    </div>
+    </>
   );
 };

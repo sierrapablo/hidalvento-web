2a72dcbe6f31ce2d03e6ce5c3537b37a3fc1ce26
sierrapablo <pablosierralorente@outlook.es>
2025-04-03 04:57:13 +0200

    🎨 code refactoring: Refactor HeroSection

--- src/components/templates/HeroSection/HeroSection.tsx
+++ src/components/templates/HeroSection/HeroSection.tsx
@@ -1,15 +1,10 @@
-import { tv } from 'tailwind-variants';
-import { HeroContent, HeroContentProps } from '../molecules/HeroContent';
-
-
-const heroSectionStyles = tv({
-  base: "bg-gray-100"
-});
+import { HeroContent, HeroContentProps } from '../../molecules/HeroContent/HeroContent';
+import { heroSectionStyles } from './heroSectionStyles';
 
 export const HeroSection = ({ subtitle, image }: HeroContentProps) => {
   return (
     <section className={heroSectionStyles()}>
       <HeroContent subtitle={subtitle} image={image} />
-      </section>
+    </section>
   );
 };
---
+++ src/components/templates/HeroSection/heroSectionStyles.ts
@@ -0,0 +1,5 @@
+import { tv } from 'tailwind-variants';
+
+export const heroSectionStyles = tv({
+  base: "bg-gray-100"
+});

453eba9d72f36cf06ae0d0fa7e0e50ae18e5100b
sierrapablo <pablosierralorente@outlook.es>
2025-04-03 05:30:56 +0200

    ✨ feature: enhance styles when hovering image grid previews

--- src/components/templates/ImageGrid/imageGridStyles.ts
+++ src/components/templates/ImageGrid/imageGridStyles.ts
@@ -1,9 +1,9 @@
 import { tv } from 'tailwind-variants';
 
 export const imageGridStyles = tv({
-  base: "grid grid-cols-3 gap-1 px-4 py-10"
+  base: "grid grid-cols-3 px-4 py-10"
 });
 
 export const imageContainerStyles = tv({
-  base: "relative w-full h-48 lg:h-96 overflow-hidden rounded-sm shadow-md cursor-pointer transition-transform duration-300 hover:scale-105"
+  base: "relative w-full h-48 lg:h-96 overflow-hidden rounded-sm cursor-pointer transition-ease-in-out transition-1000 border-2 border-white hover:border-hv-blue"
 });

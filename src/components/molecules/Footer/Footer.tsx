fe427b66ccdfb6bc30b37817302b83c4baea9b64
sierrapablo <pablosierralorente@outlook.es>
2025-05-04 10:30:44 +0200

    ⬆ major: Home PAge and contact footer Links

--- src/components/molecules/Footer/Footer.tsx
+++ src/components/molecules/Footer/Footer.tsx
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom';
 import { footerStyles, footerContainerStyles, columnContainerStyles, logoContainerStyles, iconStyles, socialsContainerStyles, labelStyles, titleStyles } from './footerStyles';
 
 export const Footer = () => {
+
+  const mapsUrl = encodeURI(
+    'https://www.google.com/maps/search/?api=1&query=Pl. Julio Verne 2, Esc. B, 7, 46017 Valencia, España'
+  );
+
   return (
     <footer className={footerStyles()}>
       <div className={footerContainerStyles()}>
@@ -24,12 +29,12 @@ export const Footer = () => {
         {/* Tarjeta de contacto */}
         <div className={columnContainerStyles()}>
           <a className={titleStyles()}>Contacto</a>
-          <a href="" className={labelStyles()}>
+          <a href={mapsUrl} target='_blank' rel='noopener noreferrer' className={labelStyles()}>
             Pl. Julio Verne 2, Esc. B, 7<br />
             46017 Valencia, España
           </a>
-          <a href="" className={labelStyles()}>+34 641 68 91 30</a>
-          <a href="" className={labelStyles()}>comercial@reformashidalvento.com</a>
+          <Link to="/contacto" className={labelStyles()}>+34 641 68 91 30</Link>
+          <Link to="/contacto" className={labelStyles()}>comercial@reformashidalvento.com</Link>
           <Link to="/contacto" className={labelStyles()}>Contacto</Link>
         </div>
 
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from '../components/molecules/Navbar/Navbar';
 import { Footer } from '../components/molecules/Footer/Footer';
 import { HeroSection } from '../components/templates/HeroSection/HeroSection';
+import './App.css';
 
 import heroImg from '../assets/images/DSC03735-Mejorado-NR.avif'
 
@@ -16,6 +17,20 @@ export const App = () => {
         }}
         image={heroImg}
       />
+      <section className='aboutContainer py-20' id='about'>
+        <div className='aboutContent'>
+          <p className='aboutHeader'>Quiénes Somos</p>
+          <p className='aboutDescription'>
+            Durante años, hemos convertido simples inmuebles en hogares a los que ahora, nuestros clientes disfrutan de aquello que antes solo imaginaba,.
+          </p>
+          <p className='aboutDescription'>
+            Una buena reforma integral es un proceso que empieza por escuchar a nuestro cliente y presentarle soluciones a medida, asesorarle en la elección de materiales de primera calidad y brindarle un abanico de profesionales que cubrirán todas sus necesidades.
+          </p>
+          <p className='aboutDescription'>
+            Pero cuando el último detalle está acabado, nuestro trabajo continúa estando a su disposición para cualquier pequeña reparación.
+          </p>
+        </div>
+      </section>
       <Footer />
     </>
   );

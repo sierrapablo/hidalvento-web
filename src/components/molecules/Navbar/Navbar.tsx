4386db8319b9b49337bff83b9082eabdf1ea2bb8
sierrapablo <pablosierralorente@outlook.es>
2025-05-21 18:00:36 +0200

    ✨ feature: New structure: Home Page based

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -5,10 +5,14 @@ export const Navbar = () => {
   return (
     <nav className={navbarStyles()}>
       <NavLink to="/" label="Inicio" />
+      {/*
       <NavLink to="/sobre-nosotros" label="Sobre Nosotros" />
       <NavLink to="/servicios" label="Servicios" />
+      */}
       <NavLink to="/galeria" label="Galería" />
+      {/*
       <NavLink to="/contacto" label="Contacto" />
+      */}
     </nav>
   );
 };
